14884ecb396379ce11afbe5bf86c515ac2d8b368
ABHAY <369abhayg@gmail.com>
2024-05-03 12:36:52 +0530

    backend code

---
+++ demo/backend/main.js
@@ -0,0 +1,40 @@
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/api/jokes',(req,res)=>{
+  const jokes = [
+    {
+      id: 1,
+      title: "Joke1",
+      content: "First Joke"
+    },
+    {
+      id: 2,
+      title: "Joke2",
+      content: "Second Joke"
+    },
+    {
+      id: 3,
+      title: "Joke3",
+      content: "Third Joke"
+    },
+    {
+      id: 4,
+      title: "Joke4",
+      content: "Fourth Joke"
+    },
+    {
+      id: 5,
+      title: "Joke5",
+      content: "Fifth Joke"
+    },
+  ]
+  res.send(jokes);
+});
+
+app.listen(port,()=>{
+  console.log(`Serve at http://localhost:${port}`);
+});
